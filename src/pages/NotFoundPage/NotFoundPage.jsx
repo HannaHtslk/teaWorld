2742470090ab67495b5db4edc04bfb1f52370aa8
AnnaHtslk <hannah.htslk@gmail.com>
2024-05-24 11:13:56 +0300

    added notFound page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import s from './NotFoundPage.module.css';
+import { TbFaceIdError } from 'react-icons/tb';
+
 const NotFoundPage = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className={s.section}>
+      <div className={s.container}>
+        <div className={s.wrapper}>
+          <TbFaceIdError className={s.icon} size="100" />
+          <h1 className={s.title}>Oops, such page does not exist</h1>
+          <Link className={s.link} to="/">
+            Home
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundPage;
